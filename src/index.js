d6da4a4a6a9baf0a5b7b8c33cb3d77fef0ebd228
mnorlin <1668384+mnorlin@users.noreply.github.com>
2021-03-20 14:59:54 +0100

    Update create-react-app

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./styling/index.scss";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import "./styling/styling.scss";
 import "bootstrap/dist/js/bootstrap.min.js";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import reportWebVitals from "./reportWebVitals";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
-serviceWorker.unregister();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
