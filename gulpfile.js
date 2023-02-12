f0c4ba6f8e592daa4f17d2d2969a59ed4ed4da3d
mnorlin <1668384+mnorlin@users.noreply.github.com>
2023-02-12 23:18:58 +0100

    chore: update release step for new react version

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,19 @@ const gulp = require("gulp");
 const inlinesource = require("gulp-inline-source");
 const replace = require("gulp-replace");
 const base64 = require("gulp-base64-inline");
+const textTransformation = require("gulp-text-simple");
+const prettier = require("prettier");
+
+function moveLine(s) {
+  const formatted = prettier.format(s, { parser: "html" });
+  const lines = formatted.split("\n");
+  const scriptIndex = lines.findIndex((line) => line.trim().startsWith("<script"));
+  const scriptLine = lines.splice(scriptIndex, 1);
+  lines.splice(scriptIndex + 6, 0, scriptLine);
+  return lines.join("\n");
+}
+
+const moveLineTransform = textTransformation(moveLine);
 
 gulp.task("default", () => {
   return gulp
@@ -11,6 +24,7 @@ gulp.task("default", () => {
     .pipe(replace('href="/img/', 'href="inline(/img/'))
     .pipe(replace('.png"/>', '.png)"/>'))
     .pipe(replace('.svg"/>', '.svg)"/>'))
+    .pipe(moveLineTransform())
     .pipe(
       base64("../public", {
         prefix: "",
